634e9117f64a14c42f264e99c33fdc0c53e68efc
ji sun lee <ellijs@hotmail.com>
2021-03-25 00:59:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // Add your doToElementsInArray() function here:
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
 
 
 // Add your changeCompletely() function here:
+function changeCompletely(element, array, index) {
+  array[index] = Math.floor(Math.random() * 100).toString() + `${array[index]}s!!!`;
+}
+
+var animals = ["dog". "cat", "squarrel"];
+
+console.log(animals);
